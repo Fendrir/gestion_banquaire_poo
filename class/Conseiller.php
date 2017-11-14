66928a59ba33ce49eb217b51c7ff17e75ed45a5f
Fendrir <papao1@hotmail.fr>
2017-11-14 11:10:38 +0100

    nouvelle indentation fichier

--- class/Conseiller.php
+++ class/Conseiller.php
@@ -2,9 +2,11 @@
 class Conseiller extends Personne
 {
   private $nbClient = 0;
+
   public function __toString()
   {
     return parent::__toString().
             " Nb Client: ".$this->nbClient."<br>";
   }
+  
 }
